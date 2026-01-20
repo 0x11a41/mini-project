7d9d75f1cae553fa47d08ef325d6fbef00d61464
Harikrishna Mohan <harikrishnamohan@proton.me>
2026-01-20 21:01:32 +0530

    server structure created

--- server/main.py
+++ server/main.py
@@ -3,26 +3,23 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-async def read_root():
-    return FileResponse("static/index.html")
 
 clients = []
-
 @app.websocket("/ws/control")
 async def handle_control_commands(ws: WebSocket):
     await ws.accept()
     clients.append(ws)
-
     try:
         while True:
             data = await ws.receive_text()
             print(f"Received: {data}")
-            
             for client in clients:
                 await client.send_text(data)
-
     except WebSocketDisconnect:
         clients.remove(ws)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/")
+async def read_root():
+    return FileResponse("static/index.html")
